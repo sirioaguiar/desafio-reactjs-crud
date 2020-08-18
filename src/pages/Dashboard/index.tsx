5a3c9dfe27d376fc31aa94991f7ae42a2bb3f257
Sírio Aguiar <sirioaguiar@gmail.com>
2020-08-17 23:24:26 -0300

    desafio finalizado

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      await api.get<IFoodPlate[]>('/foods').then(response => {
+        setFoods(response.data);
+      });
     }
 
     loadFoods();
@@ -37,7 +39,10 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const newFood = { ...food, available: true };
+      const response = await api.post('/foods', newFood);
+      const foodsUpdated = [...foods, response.data];
+      setFoods(foodsUpdated);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +51,24 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const { id, available } = editingFood;
+    if (id && available) {
+      const foodUpdated = { id, ...food, available };
+      const response = await api.put(`/foods/${id}`, foodUpdated);
+      const allFoods = foods.map(currentFood => {
+        if (currentFood.id === foodUpdated.id) {
+          return response.data;
+        }
+        return currentFood;
+      });
+      setFoods(allFoods);
+    }
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+    const allFoods = foods.filter(food => food.id !== id);
+    setFoods(allFoods);
   }
 
   function toggleModal(): void {
@@ -62,7 +80,10 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    toggleEditModal();
+
+    setEditingFood(food);
+    handleUpdateFood(food);
   }
 
   return (
